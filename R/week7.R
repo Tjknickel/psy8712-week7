088e2163784eaf1275e2870855b2c6805f19fafd
Tjknickel <knick071@umn.edu>
2026-03-13 10:06:05 -0500

    Finished data import and cleaning

--- R/week7.R
+++ R/week7.R
@@ -1,5 +1,17 @@
 # Script Settings and Resources 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+library(tidyverse)
 
 
 
+
+# Data Import and Cleaning 
+week7_tbl <- read.csv("../data/week3.csv", header = TRUE) %>%
+  mutate(across(c(timeStart, timeEnd), as.POSIXct)) %>%
+  pivot_longer(condition:gender, names_to = "Variable", values_to = "Level") %>%
+  mutate(Variable = str_replace_all(Variable, c("condition" = "Condition", "gender" = "Gender"))) %>%
+  mutate(Label = Level) %>%
+  mutate(Label = str_replace_all(Label, c("^A$" = "Block A", "^B$" = "Block B", "C" = "Control", "M" = "Male", "F" = "Female"))) %>%
+  filter(q6 == 1) %>%
+  select(-q6) %>%
+  mutate(timeSpent = difftime(timeEnd, timeStart, units = "mins"))
